2bc04c98586a630c14a79dfdcb02d107bd24598b
slamdunk7575 <slamdunk7575@gmail.com>
2021-08-09 18:54:28 +0900

    (feat) 통합 테스트에서 공통으로 사용할 클래스

---
+++ src/test/java/kitchenpos/BaseServiceTest.java
@@ -0,0 +1,18 @@
+package kitchenpos;
+
+import kitchenpos.util.DatabaseCleanup;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseServiceTest {
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    void setUp() {
+        databaseCleanup.execute();
+    }
+}
