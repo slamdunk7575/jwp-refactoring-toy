33ac996c6394737eae4353372833bd6b51f3fd0a
slamdunk7575 <slamdunk7575@gmail.com>
2021-08-11 20:54:11 +0900

    (test) MenuGroup 도메인 테스트

---
+++ src/test/java/kitchenpos/menugroup/domain/MenuGroupTest.java
@@ -0,0 +1,33 @@
+package kitchenpos.menugroup.domain;
+
+import kitchenpos.common.domain.name.InvalidNameException;
+import kitchenpos.menu.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MenuGroupTest {
+
+    @DisplayName("메뉴 그룹을 생성할 수 있다.")
+    @Test
+    void createMenuGroup() {
+        // given
+        String name = "추천메뉴";
+
+        // when
+        MenuGroup menuGroup = new MenuGroup(name);
+
+        // then
+        assertThat(menuGroup).isNotNull();
+    }
+
+    @DisplayName("메뉴 그룹 이름은 필수정보이다.")
+    @Test
+    void requireMenuGroupName() {
+        // when & then
+        assertThatThrownBy(() -> new MenuGroup(""))
+                .isInstanceOf(InvalidNameException.class).hasMessage("이름은 필수 정보입니다.");
+    }
+}
