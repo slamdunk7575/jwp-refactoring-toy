6afb7e3f150eb85c74290ae9085606bcf2c5ed97
slamdunk7575 <slamdunk7575@gmail.com>
2021-08-11 20:52:29 +0900

    (feat) MenuGroup 도메인 JPA 엔티티 설정

---
+++ src/main/java/kitchenpos/menu/domain/MenuGroup.java
@@ -0,0 +1,36 @@
+package kitchenpos.menu.domain;
+
+import kitchenpos.common.domain.name.Name;
+
+import javax.persistence.*;
+
+@Entity
+public class MenuGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Name name;
+
+    protected MenuGroup() {
+    }
+
+    public MenuGroup(Long id, String name) {
+        this.id = id;
+        this.name = new Name(name);
+    }
+
+    public MenuGroup(String name) {
+        this(null, name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.value();
+    }
+}
