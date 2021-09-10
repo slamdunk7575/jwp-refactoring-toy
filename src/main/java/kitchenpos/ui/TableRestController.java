186b831eed3f6ff20b49a51d4ed982ab3d84ffa7
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-11 02:24:11 +0900

    (refactor) TableRestController 주석 처리 (리팩토링 전)

--- src/main/java/kitchenpos/ui/TableRestController.java
+++ src/main/java/kitchenpos/ui/TableRestController.java
@@ -1,54 +1,41 @@
 package kitchenpos.ui;
 
-import kitchenpos.application.TableService;
-import kitchenpos.domain.OrderTable;
-import org.springframework.http.ResponseEntity;
+import kitchenpos.order.application.OrderTableService;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.util.List;
 
 @RestController
 public class TableRestController {
-    private final TableService tableService;
+    private final OrderTableService orderTableService;
 
-    public TableRestController(final TableService tableService) {
-        this.tableService = tableService;
+    public TableRestController(OrderTableService orderTableService) {
+        this.orderTableService = orderTableService;
     }
 
-    @PostMapping("/api/tables")
+    /*@PostMapping("/api/tables")
     public ResponseEntity<OrderTable> create(@RequestBody final OrderTable orderTable) {
-        final OrderTable created = tableService.create(orderTable);
+        final OrderTable created = orderTableService.create(orderTable);
         final URI uri = URI.create("/api/tables/" + created.getId());
         return ResponseEntity.created(uri)
-                .body(created)
-                ;
+                .body(created);
     }
 
     @GetMapping("/api/tables")
-    public ResponseEntity<List<OrderTable>> list() {
+    public ResponseEntity<List<OrderTableResponse>> list() {
         return ResponseEntity.ok()
-                .body(tableService.list())
-                ;
+                .body(orderTableService.findAll());
     }
 
     @PutMapping("/api/tables/{orderTableId}/empty")
-    public ResponseEntity<OrderTable> changeEmpty(
-            @PathVariable final Long orderTableId,
-            @RequestBody final OrderTable orderTable
-    ) {
+    public ResponseEntity<OrderTable> changeEmpty(@PathVariable final Long orderTableId,
+                                                  @RequestBody final OrderTable orderTable) {
         return ResponseEntity.ok()
-                .body(tableService.changeEmpty(orderTableId, orderTable))
-                ;
+                .body(orderTableService.changeEmpty(orderTableId, orderTable));
     }
 
     @PutMapping("/api/tables/{orderTableId}/number-of-guests")
-    public ResponseEntity<OrderTable> changeNumberOfGuests(
-            @PathVariable final Long orderTableId,
-            @RequestBody final OrderTable orderTable
-    ) {
+    public ResponseEntity<OrderTable> changeNumberOfGuests(@PathVariable final Long orderTableId,
+                                                           @RequestBody final OrderTable orderTable) {
         return ResponseEntity.ok()
-                .body(tableService.changeNumberOfGuests(orderTableId, orderTable))
-                ;
-    }
+                .body(orderTableService.changeNumberOfGuests(orderTableId, orderTable));
+    }*/
 }
