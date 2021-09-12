f3b181f1335178fe969572da5981da10683431b4
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-12 19:18:32 +0900

    (refactor) OrderTableRestController 리팩토링

---
+++ src/main/java/kitchenpos/order/ui/OrderTableRestController.java
@@ -0,0 +1,50 @@
+package kitchenpos.order.ui;
+
+import kitchenpos.order.application.OrderTableService;
+import kitchenpos.order.dto.OrderTableRequest;
+import kitchenpos.order.dto.OrderTableResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class OrderTableRestController {
+
+    private final OrderTableService orderTableService;
+
+    public OrderTableRestController(OrderTableService orderTableService) {
+        this.orderTableService = orderTableService;
+    }
+
+    @PostMapping("/api/tables")
+    public ResponseEntity<OrderTableResponse> create(@RequestBody final OrderTableRequest orderTableRequest) {
+        final OrderTableResponse created = orderTableService.create(orderTableRequest);
+        final URI uri = URI.create("/api/tables/" + created.getId());
+        return ResponseEntity.created(uri)
+                .body(created);
+    }
+
+    @GetMapping("/api/tables")
+    public ResponseEntity<List<OrderTableResponse>> findAll() {
+        return ResponseEntity.ok()
+                .body(orderTableService.findAll());
+    }
+
+    @PutMapping("/api/tables/{orderTableId}/empty")
+    public ResponseEntity<OrderTableResponse> changeEmpty(@PathVariable final Long orderTableId,
+                                                          @RequestBody final OrderTableRequest orderTableRequest) {
+        return ResponseEntity.ok()
+                .body(orderTableService.changeEmpty(orderTableId, orderTableRequest));
+    }
+
+    @PutMapping("/api/tables/{orderTableId}/number-of-guests")
+    public ResponseEntity<OrderTableResponse> changeNumberOfGuests(@PathVariable final Long orderTableId,
+                                                                   @RequestBody final OrderTableRequest orderTableRequest) {
+        return ResponseEntity.ok()
+                .body(orderTableService.changeNumberOfGuests(orderTableId, orderTableRequest));
+
+    }
+
+}
