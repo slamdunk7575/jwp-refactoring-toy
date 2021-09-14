b03bec42a3d0fbfb2bdbe62a72027fa70d9f4925
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-14 20:07:20 +0900

    (feat) Order 관련 DTO 추가

---
+++ src/main/java/kitchenpos/order/dto/OrderLineItemResponse.java
@@ -0,0 +1,52 @@
+package kitchenpos.order.dto;
+
+import kitchenpos.order.domain.OrderLineItem;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderLineItemResponse {
+    private Long id;
+    private Long orderId;
+    private Long menuId;
+    private long quantity;
+
+    protected OrderLineItemResponse() {
+    }
+
+    public OrderLineItemResponse(Long id, Long orderId, Long menuId, long quantity) {
+        this.id = id;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
+
+    public static OrderLineItemResponse of(OrderLineItem orderLineItem) {
+        return new OrderLineItemResponse(orderLineItem.getId(),
+                orderLineItem.getOrderId(),
+                orderLineItem.getMenuId(),
+                orderLineItem.getQuantity());
+    }
+
+    public static List<OrderLineItemResponse> ofList(List<OrderLineItem> orderLineItems) {
+        return orderLineItems.stream()
+                .map(OrderLineItemResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getMenuId() {
+        return menuId;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+}
---
+++ src/main/java/kitchenpos/order/dto/OrderRequest.java
@@ -0,0 +1,37 @@
+package kitchenpos.order.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class OrderRequest {
+    private Long orderTableId;
+    private String orderStatus;
+    private List<OrderLineItemRequest> orderLineItemRequests = new ArrayList<>();
+
+    public OrderRequest() {
+    }
+
+    public OrderRequest(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public OrderRequest(Long orderTableId, List<OrderLineItemRequest> orderLineItemRequests) {
+        this.orderTableId = orderTableId;
+        if (Objects.nonNull(orderLineItemRequests)) {
+            this.orderLineItemRequests = orderLineItemRequests;
+        }
+    }
+
+    public Long getOrderTableId() {
+        return orderTableId;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public List<OrderLineItemRequest> getOrderLineItemRequests() {
+        return orderLineItemRequests;
+    }
+}
