8abf47a40e2d3b6ffb619b1c0505f8c3c5178e13
slamdunk7575 <slamdunk7575@gmail.com>
2021-10-08 23:40:06 +0900

    (feat) Order -> OrderTable 양방향을 단방향으로 수정

--- src/main/java/kitchenpos/order/domain/OrderTable.java
+++ src/main/java/kitchenpos/order/domain/OrderTable.java
@@ -19,9 +19,6 @@ public class OrderTable {
     @Column(name = "empty")
     private boolean empty;
 
-    @Embedded
-    private Orders orders = new Orders();
-
     protected OrderTable() {
     }
 
@@ -51,33 +48,12 @@ public class OrderTable {
         this.empty = false;
     }
 
-    public boolean isNotComplete() {
-        return orders.hasNotComplete();
-    }
-
     public void unGroup() {
-        if (orders.hasNotComplete()) {
-            throw new IllegalArgumentException("주문이 완료되지 않은 테이블은 그룹해제 할 수 없습니다.");
-        }
         this.tableGroupId = null;
     }
 
     public void updateEmpty(boolean empty) {
-        checkOrderTableGroup();
-        checkOrderTableStatus();
         this.empty = empty;
-    }
-
-    private void checkOrderTableStatus() {
-        if (isNotComplete()) {
-            throw new IllegalArgumentException("주문 상태가 조리중 또는 식사중인 주문 테이블의 상태는 변경할 수 없습니다.");
-        }
-    }
-
-    private void checkOrderTableGroup() {
-        if (Objects.nonNull(tableGroupId)) {
-            throw new IllegalArgumentException("그룹 지정이 되어있어 상태를 변경할 수 없습니다.");
-        }
     }
 
     public void updateNumberOfGuests(int numberOfGuests) {
