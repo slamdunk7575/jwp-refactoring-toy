0a4a10f4ed861b0bb2d02621d91d2b4dd8e87232
slamdunk7575 <slamdunk7575@gmail.com>
2021-10-19 23:31:54 +0900

    (test) Menu 검증 테스트 추가

--- src/test/java/kitchenpos/menu/application/MenuValidatorTest.java
+++ src/test/java/kitchenpos/menu/application/MenuValidatorTest.java
@@ -12,10 +12,12 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static kitchenpos.util.TestFixture.*;
 import static kitchenpos.util.TestFixture.메뉴상품_양념;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuValidatorTest {
 
@@ -51,7 +53,7 @@ class MenuValidatorTest {
 
         // when & then
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-            menuValidator.validateMenuPrice(menu, new Price(wrongPrice));
+            menuValidator.validateMenuPrice(menu, new Price(totalProductPrice));
         }).withMessageMatching("메뉴 가격이 속한 상품들 가격 합보다 비쌉니다.");
     }
 
@@ -62,5 +64,16 @@ class MenuValidatorTest {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
             menuValidator.validateMenuProducts(new ArrayList<>());
         }).withMessageMatching("메뉴에 1개 이상의 상품이 포함되어야 합니다.");
+    }
+
+    @DisplayName("메뉴 그룹이 등록되어 있지 않으면 메뉴를 생성할 수 없다.")
+    @Test
+    void notExistMenuGroup() {
+        Optional<MenuGroup> emptyMenuGroup = Optional.empty();
+
+        assertThatThrownBy(() -> {
+            menuValidator.validateExistMenuGroup(emptyMenuGroup);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("등록되지 않은 메뉴 그룹 입니다.");
     }
 }
